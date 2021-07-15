e9cd12ec71aa077c40bccdb0d4707972db620e19
gautier HAMON <gautier.hamon@telecom-paristech.fr>
2021-07-15 12:23:30 +0200

    test slider

--- gl.js
+++ gl.js
@@ -449,14 +449,8 @@ function set_iMouse(e, sx, sy) {
 }
 function onMouseDown(e) { isMouseDown = true;  set_iMouse(e, +1, +1); }
 function onMouseMove(e) { if (isMouseDown)     set_iMouse(e, +1, -1); }
+function onMouseUp  (e) { isMouseDown = false; set_iMouse(e, -1, -1);}
 
-/*
-function onMouseUp  (e) { isMouseDown = false; set_iMouse(e, -1, -1);
-  randomSpecies = Math.floor(new Date().getSeconds() / 60 * 10); //Math.floor(Math.random() * 9);
-  initSpecies = getParam("species", "int", randomSpecies, randomSpecies);
-  setSpecies(initSpecies);
-}
-*/
 
 
 var sliderRadius = document.getElementById("rangeRadius");
